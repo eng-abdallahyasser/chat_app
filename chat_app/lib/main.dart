e52ef329b4747f120928ab9536de9c1364839a41
ENG ABDALLAH YASSER <abdallahyasser1277@gmail.com>
2024-06-01 00:16:29 +0300

    "Updated main.dart to use Home screen instead of SignIn screen, and added assets/images/ to pubspec.yaml"

--- chat_app/lib/main.dart
+++ chat_app/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:chat_app/presentation/screens/sign_in_screen.dart';
+import 'package:chat_app/presentation/screens/home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -33,7 +33,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const SignIn(),
+      home:  Home(),
     );
   }
 }
